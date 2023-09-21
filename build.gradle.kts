83be08b35079933d44b33af9e44bb87ed1825e20
James Ward <james@jamesward.com>
2023-09-21 14:58:05 -0600

    try maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     `kotlin-dsl`
     `maven-publish`
+    signing
     id("com.gradle.plugin-publish") version "1.2.1"
     id("me.qoomon.git-versioning") version "6.4.2"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 repositories {
@@ -17,16 +19,21 @@ gitVersioning.apply {
     }
 }
 
+val pluginName = "Kotlin Universe Catalog"
+val pluginDescription = "Gradle convention plugin that defines version catalogs for the universe of Kotlin Gradle plugins and libraries"
+val pluginUrl = "https://github.com/jamesward/kotlin-universe-catalog"
+val pluginScm = "scm:git:git@github.com:jamesward/kotlin-universe-catalog.git"
+
 @Suppress("UnstableApiUsage")
 gradlePlugin {
-    website = "https://github.com/jamesward/kotlin-universe-catalog"
-    vcsUrl = "https://github.com/jamesward/kotlin-universe-catalog"
+    website = pluginUrl
+    vcsUrl = pluginUrl
     plugins {
         create("KotlinUniverseCatalog") {
             id = "com.jamesward.kotlin-universe-catalog"
             implementationClass = "com.jamesward.kotlinuniversecatalog.GradlePlugin"
-            displayName = "Kotlin Universe Catalog"
-            description = "Gradle convention plugin that defines version catalogs for the universe of Kotlin Gradle plugins and libraries"
+            displayName = pluginName
+            description = pluginDescription
             tags = listOf("kotlin")
         }
     }
@@ -34,4 +41,54 @@ gradlePlugin {
 
 kotlin {
     jvmToolchain(8)
+}
+
+@Suppress("UnstableApiUsage")
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name = pluginName
+                description = pluginDescription
+                url = pluginUrl
+
+                scm {
+                    connection = "scm:git:$pluginUrl.git"
+                    developerConnection = pluginScm
+                    url = pluginUrl
+                }
+
+                licenses {
+                    license {
+                        name = "Apache 2.0"
+                        url = "https://opensource.org/licenses/Apache-2.0"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "jamesward"
+                        name = "James Ward"
+                        email = "james@jamesward.com"
+                        url = "https://jamesward.com"
+                    }
+                }
+            }
+
+            from(components["java"])
+        }
+    }
+}
+
+@Suppress("UnstableApiUsage")
+nexusPublishing.repositories {
+    sonatype {
+        username = System.getenv("SONATYPE_USERNAME")
+        password = System.getenv("SONATYPE_PASSWORD")
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+    useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSPHRASE"))
 }
