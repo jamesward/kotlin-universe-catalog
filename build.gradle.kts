768bef15a2ada1a5cf350bb6bcb20f7eaa0dd8c6
James Ward <james@jamesward.com>
2023-09-21 15:49:39 -0600

    try again

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,37 @@ kotlin {
 }
 
 @Suppress("UnstableApiUsage")
+publishing {
+    publications {
+        configureEach {
+            (this as MavenPublication).pom {
+                scm {
+                    connection = "scm:git:$pluginUrl.git"
+                    developerConnection = pluginScm
+                    url = pluginUrl
+                }
+
+                licenses {
+                    license {
+                        name = "Apache 2.0"
+                        url = "https://opensource.org/licenses/Apache-2.0"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "jamesward"
+                        name = "James Ward"
+                        email = "james@jamesward.com"
+                        url = "https://jamesward.com"
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Suppress("UnstableApiUsage")
 nexusPublishing.repositories {
     sonatype {
         username = System.getenv("SONATYPE_USERNAME")
